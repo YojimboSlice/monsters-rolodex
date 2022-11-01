b07a0e44cf0abd19fb98371f201b811b50b765a1
James Yan Lan <jamesyanlan@Jamess-MacBook-Pro-2.local>
2022-11-01 12:07:13 -0700

    All project files

---
+++ src/components/card-list/card-list.component.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import CardContainer from '../card-container/card-container.component';
+
+import './card-list.styles.css';
+
+const CardList = ({ monsters }) => (
+  <div className='card-list'>
+    {monsters.map(monster => {
+      return <CardContainer monster={monster} />;
+    })}
+  </div>
+);
+
+export default CardList;
